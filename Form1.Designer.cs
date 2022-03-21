f13235b60b2475c1a07c4ef66e8a0954d124a45b
Daniel21Villarreal <92354716+Daniel21Villarreal@users.noreply.github.com>
2022-03-21 15:48:44 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace reproductor_de_musica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Reproductor = new AxWMPLib.AxWindowsMediaPlayer();
+            this.lstCanciones = new System.Windows.Forms.ListBox();
+            this.mtrackEstatus = new XComponent.SliderBar.MACTrackBar();
+            this.mtrackVolumen = new XComponent.SliderBar.MACTrackBar();
+            this.lblCancion = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnStop = new System.Windows.Forms.PictureBox();
+            this.btnPlay = new System.Windows.Forms.PictureBox();
+            this.btnAdjuntar = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Reproductor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnStop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPlay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAdjuntar)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Reproductor
+            // 
+            this.Reproductor.Enabled = true;
+            this.Reproductor.Location = new System.Drawing.Point(346, 6);
+            this.Reproductor.Name = "Reproductor";
+            this.Reproductor.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Reproductor.OcxState")));
+            this.Reproductor.Size = new System.Drawing.Size(494, 219);
+            this.Reproductor.TabIndex = 0;
+            this.Reproductor.PlaylistChange += new AxWMPLib._WMPOCXEvents_PlaylistChangeEventHandler(this.Reproductor_PlaylistChange);
+            // 
+            // lstCanciones
+            // 
+            this.lstCanciones.FormattingEnabled = true;
+            this.lstCanciones.Location = new System.Drawing.Point(12, 12);
+            this.lstCanciones.Name = "lstCanciones";
+            this.lstCanciones.Size = new System.Drawing.Size(328, 95);
+            this.lstCanciones.TabIndex = 1;
+            this.lstCanciones.SelectedIndexChanged += new System.EventHandler(this.lstCanciones_SelectedIndexChanged);
+            // 
+            // mtrackEstatus
+            // 
+            this.mtrackEstatus.BackColor = System.Drawing.Color.Transparent;
+            this.mtrackEstatus.BorderColor = System.Drawing.SystemColors.ActiveBorder;
+            this.mtrackEstatus.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtrackEstatus.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(125)))), ((int)(((byte)(123)))));
+            this.mtrackEstatus.IndentHeight = 6;
+            this.mtrackEstatus.Location = new System.Drawing.Point(83, 10);
+            this.mtrackEstatus.Maximum = 10;
+            this.mtrackEstatus.Minimum = 0;
+            this.mtrackEstatus.Name = "mtrackEstatus";
+            this.mtrackEstatus.Size = new System.Drawing.Size(411, 22);
+            this.mtrackEstatus.TabIndex = 3;
+            this.mtrackEstatus.TextTickStyle = System.Windows.Forms.TickStyle.None;
+            this.mtrackEstatus.TickColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(146)))), ((int)(((byte)(148)))));
+            this.mtrackEstatus.TickHeight = 4;
+            this.mtrackEstatus.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.mtrackEstatus.TrackerColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(130)))), ((int)(((byte)(198)))));
+            this.mtrackEstatus.TrackerSize = new System.Drawing.Size(10, 10);
+            this.mtrackEstatus.TrackLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(93)))), ((int)(((byte)(90)))));
+            this.mtrackEstatus.TrackLineHeight = 3;
+            this.mtrackEstatus.TrackLineSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(93)))), ((int)(((byte)(90)))));
+            this.mtrackEstatus.Value = 0;
+            // 
+            // mtrackVolumen
+            // 
+            this.mtrackVolumen.BackColor = System.Drawing.Color.Transparent;
+            this.mtrackVolumen.BorderColor = System.Drawing.SystemColors.ActiveBorder;
+            this.mtrackVolumen.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtrackVolumen.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(125)))), ((int)(((byte)(123)))));
+            this.mtrackVolumen.IndentHeight = 6;
+            this.mtrackVolumen.Location = new System.Drawing.Point(52, 149);
+            this.mtrackVolumen.Maximum = 100;
+            this.mtrackVolumen.Minimum = 0;
+            this.mtrackVolumen.Name = "mtrackVolumen";
+            this.mtrackVolumen.Size = new System.Drawing.Size(276, 22);
+            this.mtrackVolumen.TabIndex = 4;
+            this.mtrackVolumen.TextTickStyle = System.Windows.Forms.TickStyle.None;
+            this.mtrackVolumen.TickColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(146)))), ((int)(((byte)(148)))));
+            this.mtrackVolumen.TickHeight = 4;
+            this.mtrackVolumen.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.mtrackVolumen.TrackerColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(130)))), ((int)(((byte)(198)))));
+            this.mtrackVolumen.TrackerSize = new System.Drawing.Size(10, 10);
+            this.mtrackVolumen.TrackLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(93)))), ((int)(((byte)(90)))));
+            this.mtrackVolumen.TrackLineHeight = 3;
+            this.mtrackVolumen.TrackLineSelectedColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(93)))), ((int)(((byte)(90)))));
+            this.mtrackVolumen.Value = 0;
+            this.mtrackVolumen.ValueChanged += new XComponent.SliderBar.ValueChangedHandler(this.mtrackVolumen_ValueChanged);
+            // 
+            // lblCancion
+            // 
+            this.lblCancion.AutoSize = true;
+            this.lblCancion.Location = new System.Drawing.Point(12, 190);
+            this.lblCancion.Name = "lblCancion";
+            this.lblCancion.Size = new System.Drawing.Size(16, 13);
+            this.lblCancion.TabIndex = 8;
+            this.lblCancion.Text = "---";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(12, 146);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(34, 31);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStop.Image = global::reproductor_de_musica.Properties.Resources.stop;
+            this.btnStop.Location = new System.Drawing.Point(45, 6);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(34, 32);
+            this.btnStop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnStop.TabIndex = 6;
+            this.btnStop.TabStop = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlay.Image = ((System.Drawing.Image)(resources.GetObject("btnPlay.Image")));
+            this.btnPlay.Location = new System.Drawing.Point(5, 6);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(34, 32);
+            this.btnPlay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnPlay.TabIndex = 5;
+            this.btnPlay.TabStop = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnAdjuntar
+            // 
+            this.btnAdjuntar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdjuntar.Image = ((System.Drawing.Image)(resources.GetObject("btnAdjuntar.Image")));
+            this.btnAdjuntar.Location = new System.Drawing.Point(846, 9);
+            this.btnAdjuntar.Name = "btnAdjuntar";
+            this.btnAdjuntar.Size = new System.Drawing.Size(30, 22);
+            this.btnAdjuntar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnAdjuntar.TabIndex = 2;
+            this.btnAdjuntar.TabStop = false;
+            this.btnAdjuntar.Click += new System.EventHandler(this.btnAdjuntar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.mtrackEstatus);
+            this.panel1.Controls.Add(this.btnPlay);
+            this.panel1.Controls.Add(this.btnStop);
+            this.panel1.Location = new System.Drawing.Point(346, 180);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(494, 48);
+            this.panel1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Volumen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(888, 275);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblCancion);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.mtrackVolumen);
+            this.Controls.Add(this.btnAdjuntar);
+            this.Controls.Add(this.lstCanciones);
+            this.Controls.Add(this.Reproductor);
+            this.Name = "Form1";
+            this.Text = "ReproductorMP3";
+            ((System.ComponentModel.ISupportInitialize)(this.Reproductor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnStop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPlay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAdjuntar)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Reproductor;
+        private System.Windows.Forms.ListBox lstCanciones;
+        private System.Windows.Forms.PictureBox btnAdjuntar;
+        private XComponent.SliderBar.MACTrackBar mtrackEstatus;
+        private XComponent.SliderBar.MACTrackBar mtrackVolumen;
+        private System.Windows.Forms.PictureBox btnPlay;
+        private System.Windows.Forms.PictureBox btnStop;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lblCancion;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reproductor_de_musica
+{
+    public partial class Form1 : Form
+    {
+        bool Play = false;
+        string[] ArchivosMP3;
+        string[] rutasArchivosMP3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdjuntar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog CajaDeBusquedaDeArchivos = new OpenFileDialog();
+            CajaDeBusquedaDeArchivos.Multiselect = true;
+            if(CajaDeBusquedaDeArchivos.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+            {
+                ArchivosMP3 = CajaDeBusquedaDeArchivos.SafeFileNames;
+                rutasArchivosMP3 = CajaDeBusquedaDeArchivos.FileNames;
+                foreach(var ArchivoMp3 in ArchivosMP3)
+                {
+                    lstCanciones.Items.Add(ArchivoMp3);
+
+                }
+                Reproductor.URL = rutasArchivosMP3[0];
+                lstCanciones.SelectedIndex = 0;
+                btnPlay.Image = Properties.Resources.pausa;
+            }
+        }
+
+        private void lstCanciones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Reproductor.URL = rutasArchivosMP3[lstCanciones.SelectedIndex];
+            lblCancion.Text = ArchivosMP3[lstCanciones.SelectedIndex];
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            switch(Play)
+            {
+                case true:
+                    Reproductor.Ctlcontrols.pause();
+                    btnPlay.Image = Properties.Resources.play;
+                    Play = false;
+                    break;
+                case false:
+                    Reproductor.Ctlcontrols.play();
+                    btnPlay.Image = Properties.Resources.pausa;
+                    Play = true;
+                    break;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Reproductor.Ctlcontrols.stop();
+            btnPlay.Image = Properties.Resources.play;
+            Play = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ActualizarDatosTrack();
+            mtrackEstatus.Value = (int)Reproductor.Ctlcontrols.currentPosition;
+            mtrackVolumen.Value = Reproductor.settings.volume;
+
+        }
+        public void ActualizarDatosTrack()
+        {
+            if(Reproductor.playState==WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                //control del tiempo maximo del mp3 actual
+                mtrackEstatus.Maximum=(int)Reproductor.Ctlcontrols.currentItem.duration;
+                timer1.Start();
+            }
+
+            else if (Reproductor.playState == WMPLib.WMPPlayState.wmppsPaused)
+            {
+                timer1.Stop();
+            }
+
+            else if (Reproductor.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                timer1.Stop();
+                mtrackEstatus.Value = 0;
+            }
+        }
+
+        private void Reproductor_PlaylistChange(object sender, AxWMPLib._WMPOCXEvents_PlaylistChangeEvent e)
+        {
+            ActualizarDatosTrack();
+        }
+
+        private void mtrackVolumen_ValueChanged(object sender, decimal value)
+        {
+            Reproductor.settings.volume = mtrackVolumen.Value;
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
